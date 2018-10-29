ef7a1c66f57cfb4418ce6b0f7b11729fae58e6f0
vr3dina <vr3dina@gmail.com>
2018-10-29 16:47:27 +0300

    add projects with tests and core

---
+++ EM.Calc.Core/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace EM.Calc.Core
+{
+    public class Calc
+    {
+        public int Sum(int[] args)
+        {
+            return args.Sum();
+        }
+    }
+}
---
+++ EM.Calc.Tetsts/CalcTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EM.Calc.Tetsts
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            //arrange
+            var calc = new Core.Calc();
+            var sum = 10;
+
+            //act
+            var result = calc.Sum(new[] { 5, 3, 2 });
+
+            //assert
+            Assert.AreEqual(sum, result);
+        }
+    }
+}
